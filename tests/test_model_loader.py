598762930773f1fc713769e505266f55f1809fcb
João Taube <163299922+Taube0@users.noreply.github.com>
2024-09-25 21:20:34 -0300

    Update test_model_loader.py

--- tests/test_model_loader.py
+++ tests/test_model_loader.py
@@ -1,6 +1,9 @@
 import pytest
-from model_loader import load_model
+import sys
 import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from model_loader import load_model
+
 
 def test_load_existing_model():
     """
